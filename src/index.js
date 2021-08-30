ae69da65ceb762a87cd4b4e502d2a0b3ebea0e81
Guy Paterson-Jones <guy.paterson.jones@gmail.com>
2021-08-30 10:07:02 +0200

    Added vertex and edge addition.

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import cytoscape from 'cytoscape';
 
 let BLACK = "black";
 let WHITE = "white";
+let UUID = 1;
 
 let u = vertex('u');
 let v = vertex('v');
@@ -13,12 +14,10 @@ let w = vertex('w');
 let x = vertex('x');
 
 let g = graph([u, v, w, x], [])
-  .addEdge(edge('uv', u, v))
-  .addEdge(edge('uw', u, w))
-  .addEdge(edge('ux', u, x))
-  .addEdge(edge('vw', v, w))
-  .addEdge(edge('vx', v, x))
-  .addEdge(edge('wx', w, x));
+  .addEdge(u, v)
+  .addEdge(u, w)
+  .addEdge(u, x)
+  .addEdge(w, x);
 
 let c = colouring(g, {
   [u.id]: WHITE,
@@ -39,6 +38,7 @@ let cy = cytoscape({
         'border-color': (elem) => elem.selected() ? 'blue' : 'black',
         'border-width': (elem) => elem.selected() ? '2px' : '1px',
         'background-color': 'gray',
+        'label': 'data(id)'
       }
     },
     {
@@ -56,6 +56,8 @@ let cy = cytoscape({
   ]
 });
 
+cy.on('tap', createVertexOnTap);
+
 // +-----------------------------+
 // |    DATA STRUCTURES          |
 // +-----------------------------+
@@ -63,6 +65,14 @@ let cy = cytoscape({
 function vertex(id) {
   return {
     id
+  };
+}
+
+function vertexAt(id, x, y) {
+  return {
+    id,
+    x,
+    y
   };
 }
 
@@ -74,6 +84,10 @@ function edge(id, source, target) {
 
     contains: function(vertex) {
       return this.source.id == vertex.id || this.target.id == vertex.id;
+    },
+
+    connects: function(vertexA, vertexB) {
+      return this.contains(vertexA) && this.contains(vertexB);
     },
 
     other: function(vertex) {
@@ -93,11 +107,34 @@ function graph(vertices, edges) {
     edges,
 
     addVertex: function(vertex) {
+      if (this.containsVertex(vertex)) {
+        return this;
+      }
+
       return graph([...this.vertices, vertex], this.edges);
     },
 
-    addEdge: function(edge) {
-      return graph(this.vertices, [...this.edges, edge]);
+    containsVertex: function(vertex) {
+      return vertex.id in this.vertices.map(v => v.id);
+    },
+
+    addEdge: function(vertexA, vertexB) {
+      if (this.containsEdge(vertexA, vertexB)) {
+        return this;
+      }
+
+      let e = edge(`${vertexA.id}_${vertexB.id}`, vertexA, vertexB);
+      return graph(this.vertices, [...this.edges, e]);
+    },
+
+    containsEdge: function(vertexA, vertexB) {
+      for (let edge of this.edges) {
+        if (edge.connects(vertexA, vertexB)) {
+          return true;
+        }
+      }
+
+      return false;
     }
   };
 }
@@ -135,17 +172,36 @@ function colouring(graph, colours) {
       return colouring(this.graph, colours);
     },
 
+    connect: function(vertexA, vertexB) {
+      return colouring(this.graph.addEdge(vertexA, vertexB), this.colours);
+    },
+
+    add: function(colour, x, y) {
+      let vertex = vertexAt(getUUID(), x, y);
+      let graph = this.graph.addVertex(vertex);
+      return colouring(graph, { ...this.colours, [vertex.id]: colour });
+    },
+
     toCytoscape: function() {
       let elements = [];
 
       for (let vertex of this.graph.vertices) {
-        elements.push({
+        let data = {
           data: {
             id: vertex.id
           },
 
           classes: [this.colours[vertex.id]],
-        });
+        };
+
+        if (vertex.x !== undefined && vertex.y !== undefined) {
+          data.position = {
+            x: vertex.x,
+            y: vertex.y
+          };
+        }
+
+        elements.push(data);
       }
 
       for (let edge of this.graph.edges) {
@@ -164,7 +220,7 @@ function colouring(graph, colours) {
 }
 
 // +-----------------------------+
-// |    UTILITIES AND HOOKS      |
+// |    UTILITIES                |
 // +-----------------------------+
 
 function mutateColouring(fn) {
@@ -174,11 +230,11 @@ function mutateColouring(fn) {
   }
 
   c = fn(c);
+  cy.data(c);
+  cy.add(c.toCytoscape());
   for (let vertex of c.graph.vertices) {
     cy.$('#' + vertex.id).addClass(c.colours[vertex.id]);
   }
-
-  cy.data(c);
 }
 
 function getSelectedVertices() {
@@ -203,7 +259,57 @@ function colourSelectedVertices(colour) {
   };
 }
 
+function unselectSelected() {
+  for (let elem of [...cy.nodes(), ...cy.edges()]) {
+    if (elem.selected()) {
+      elem.unselect();
+    }
+  }
+}
+
+function connectSelectedVertices() {
+  return (c) => {
+    let vertices = getSelectedVertices();
+    for (let i = 0; i < vertices.length; i++) {
+      for (let j = i+1; j < vertices.length; j++) {
+        c = c.connect(vertices[i], vertices[j]);
+      }
+    }
+
+    return c;
+  };
+}
+
+function createVertexOnTap(event) {
+  if (event.target != cy) {
+    return; // user clicked on a vertex, not the background
+  }
+
+  mutateColouring((c) => c.add(BLACK, event.position.x, event.position.y));
+  unselectSelected();
+}
+
+function getUUID() {
+  return `v${UUID++}`;
+}
+
+// +-----------------------------+
+// |   HOOKS                     |
+// +-----------------------------+
+
 window.WHITE = WHITE;
 window.BLACK = BLACK;
-window.colourSelected = (colour) => mutateColouring(colourSelectedVertices(colour));
-window.updateColouring = () => mutateColouring((c) => c.updateColouring());
+
+window.colourSelected = (colour) => {
+  mutateColouring(colourSelectedVertices(colour));
+  unselectSelected();
+}
+
+window.connectSelected = () => {
+  mutateColouring(connectSelectedVertices());
+  unselectSelected();
+}
+
+window.updateColouring = () => {
+  mutateColouring((c) => c.updateColouring());
+}
